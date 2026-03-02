4cc7b081fa90615dbc93d3b3e997b1adcab5d8a2
fisxr <alex0522.halasz@gmail.com>
2026-03-02 12:19:25 +0100

    Hibák javítása

--- main.cpp
+++ main.cpp
@@ -4,24 +4,24 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];           // NELEMENTS nem létezik, N_ELEMENTS a helyes változó
-    std::cout << '1-100 ertekek duplazasa' //  egyszeres idézőjel helyett kettős kell ("), hiányzik: << std::endl és a pontosvessző
-        for (int i = 0;)                   // hiányos a for ciklus, kell hozzá feltétel (i < N_ELEMENTS) és léptető (i++)
+    int *b = new int[N_ELEMENTS];
+    std::cout << "1-100 ertekek duplazasa" << std::endl;
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
         b[i] = i * 2;
     }
-    for (int i = 0; i; i++) // i helyett i < N_ELEMENTS kellene
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
-        std::cout << "Ertek:" // hiányzik << b[i] << std::endl és a pontosvessző
+        std::cout << "Ertek:" << b[i] << std::endl;
     }
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;                           // nincs a váltoónak érték adva, int atlag = 0;
-    for (int i = 0; i < N_ELEMENTS, i++) // vessző helyett pontosvessző kell
+    int atlag = 0;
+    for (int i = 0; i < N_ELEMENTS; i++)
     {
-        atlag += b[i] // hiányzó pontosvessző
+        atlag += b[i];
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
-    // hiányzó 'delete[] b;' - memóriaszivárgás
+    delete[] b;
     return 0;
 }
